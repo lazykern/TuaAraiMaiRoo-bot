e5c3526e58efb12656659cb58e39494f45a89c26
Phusit Somboonyingsuk <phusitsom@gmail.com>
2021-08-17 21:29:34 +0700

    Change webhook file name

--- scripts/stop_bot.sh
+++ scripts/stop_bot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/home/ubuntu/adam/envs/discord/bin/python scripts/info_offline.py
+/home/ubuntu/adam/envs/discord/bin/python scripts/webhook_info_offline.py
 
 sudo kill $(ps aux | grep python | grep main.py | awk '{print $2}') || echo 'nohup is not running'
